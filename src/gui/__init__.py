e88c1b7add4fb8363b8d7954611d1cc4793ecb3a
sam <harutokanefuji@gmail.com>
2026-09-18 04:23:33 +0900

    GUIモジュールの初期化ファイルを新規作成し、主要コンポーネントを公開

---
+++ src/gui/__init__.py
@@ -0,0 +1,11 @@
+from src.gui.main_window import MainWindow
+from src.gui.progress_panel import ProgressPanel
+from src.gui.log_viewer import LogViewerWidget
+from src.gui.settings_dialog import SettingsDialog
+
+__all__ = [
+    "MainWindow",
+    "ProgressPanel",
+    "LogViewerWidget",
+    "SettingsDialog",
+]
