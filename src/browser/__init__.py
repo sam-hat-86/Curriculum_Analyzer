aee85800d8f4ef3d49629a5fd7520c2019dc2259
sam <harutokanefuji@gmail.com>
2026-09-18 04:21:38 +0900

    ブラウザモジュールの初期化ファイルを新規作成し、クッキー抽出およびウェブエンジンビューを公開

---
+++ src/browser/__init__.py
@@ -0,0 +1,7 @@
+from src.browser.cookie_extractor import extract_cookies_sync
+from src.browser.web_engine_view import PersistentWebEngineView
+
+__all__ = [
+    "extract_cookies_sync",
+    "PersistentWebEngineView",
+]
