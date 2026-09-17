2792ac30060ef0ebcbcb75d556297109f24a480b
sam <harutokanefuji@gmail.com>
2026-09-18 04:25:36 +0900

    conftest.pyを新規作成し、プロジェクトルートをPythonパスに追加する機能を実装

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# プロジェクトルートをPythonパスに追加
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
