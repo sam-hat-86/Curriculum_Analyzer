1acbbf49790acee7acec5b4f6ad0feb788553b00
sam <harutokanefuji@gmail.com>
2026-09-18 04:24:08 +0900

    モデルモジュールの初期化ファイルを新規作成し、主要コンポーネントを公開

---
+++ src/models/__init__.py
@@ -0,0 +1,21 @@
+from src.models.state import ProcessState, TimingCategory
+from src.models.curriculum import (
+    CurriculumOverview,
+    TargetInfo,
+    UnitRecord,
+    TargetProgress,
+    CurriculumDetailData,
+)
+from src.models.evaluation import EvaluationIssue, InstructionEvaluation
+
+__all__ = [
+    "ProcessState",
+    "TimingCategory",
+    "CurriculumOverview",
+    "TargetInfo",
+    "UnitRecord",
+    "TargetProgress",
+    "CurriculumDetailData",
+    "EvaluationIssue",
+    "InstructionEvaluation",
+]
