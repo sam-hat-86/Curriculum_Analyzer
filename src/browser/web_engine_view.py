5cc41a17cd71f735b9021534e1fdf677a574ed2b
sam <harutokanefuji@gmail.com>
2026-09-18 23:59:37 +0900

    add: [web_engine_view.py] 証明書エラーを自動受諾するカスタムページクラスを追加し、コメントを更新

--- src/browser/web_engine_view.py
+++ src/browser/web_engine_view.py
@@ -6,6 +6,12 @@ from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWebEngineCore import QWebEngineProfile, QWebEnginePage, QWebEngineSettings
 from PySide6.QtCore import QUrl
 
+class CustomWebEnginePage(QWebEnginePage):
+    def certificateError(self, certificateError):
+        # 自己署名証明書等のエラーを自動受諾して表示を許可
+        certificateError.acceptCertificate()
+        return True
+
 class PersistentWebEngineView(QWebEngineView):
     def __init__(self, profile_path: str = "user_data/web_profile", parent=None):
         super().__init__(parent)
@@ -23,11 +29,11 @@ class PersistentWebEngineView(QWebEngineView):
         settings.setAttribute(QWebEngineSettings.WebAttribute.LocalStorageEnabled, True)
         settings.setAttribute(QWebEngineSettings.WebAttribute.AutoLoadImages, True)
 
-        # ページ作成
-        self.custom_page = QWebEnginePage(self.custom_profile, self)
+        # ページ作成 (証明書エラー自動受諾)
+        self.custom_page = CustomWebEnginePage(self.custom_profile, self)
         self.setPage(self.custom_page)
 
     def get_cookies_list(self) -> list:
-        """Cookieリストを抽出"""
+        """Cookieリストを抽出 (必ずメインスレッドから呼出)"""
         from src.browser.cookie_extractor import extract_cookies_sync
         return extract_cookies_sync(self.custom_profile)
